58ad8bcf8beac137faac3d18509a042cefb8a3eb
Flash <88150931+flashasdbaksdgi@users.noreply.github.com>
2021-08-10 21:18:28 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,82 @@
+import streamlit as st
+import pandas as pd
+from PIL import Image
+import subprocess
+import os
+import base64
+import pickle
+
+# Molecular descriptor calculator
+def desc_calc():
+    # Performs the descriptor calculation
+    bashCommand = "java -Xms2G -Xmx2G -Djava.awt.headless=true -jar ./PaDEL-Descriptor/PaDEL-Descriptor.jar -removesalt -standardizenitro -fingerprints -descriptortypes ./PaDEL-Descriptor/PubchemFingerprinter.xml -dir ./ -file descriptors_output.csv"
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    os.remove('molecule.smi')
+
+# File download
+def filedownload(df):
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
+    href = f'<a href="data:file/csv;base64,{b64}" download="prediction.csv">Download Predictions</a>'
+    return href
+
+# Model building
+def build_model(input_data):
+    # Reads in saved regression model
+    load_model = pickle.load(open('tox_model.pkl', 'rb'))
+    # Apply model to make predictions
+    prediction = load_model.predict(input_data)
+    st.header('**鱼毒性预测值如下**')
+    prediction_output = pd.Series(prediction, name='pLC50')
+    molecule_name = pd.Series(load_data[1], name='molecule_name')
+    df = pd.concat([molecule_name, prediction_output], axis=1)
+    st.write(df)
+    st.markdown(filedownload(df), unsafe_allow_html=True)
+
+# Logo image
+image = Image.open('logo.png')
+
+st.image(image, use_column_width=True)
+
+# Page title
+st.markdown("""
+# 农药鱼毒性预测app
+
+
+""")
+
+# Sidebar
+with st.sidebar.header('1.上传你的 CSV 数据'):
+    uploaded_file = st.sidebar.file_uploader("上传你的数据", type=['txt'])
+    st.sidebar.markdown("""
+[可以先试试预测模板](https://raw.githubusercontent.com/dataprofessor/bioactivity-prediction-app/main/example_acetylcholinesterase.txt)
+""")
+
+if st.sidebar.button('开始预测'):
+    load_data = pd.read_table(uploaded_file, sep=' ', header=None)
+    load_data.to_csv('molecule.smi', sep = '\t', header = False, index = False)
+
+    st.header('**原始输入数据**')
+    st.write(load_data)
+
+    with st.spinner("计算分子描述符..."):
+        desc_calc()
+
+    # Read in calculated descriptors and display the dataframe
+    st.header('**计算好的分子描述符**')
+    desc = pd.read_csv('descriptors_output.csv')
+    st.write(desc)
+    st.write(desc.shape)
+
+    # Read descriptor list used in previously built model
+    st.header('**之前构建好的模型**')
+    Xlist = list(pd.read_csv('descriptor_list.csv').columns)
+    desc_subset = desc[Xlist]
+    st.write(desc_subset)
+    st.write(desc_subset.shape)
+
+    # Apply trained model to make prediction on query compounds
+    build_model(desc_subset)
+else:
+    st.info('在侧边栏上传数据开始预测吧！')
