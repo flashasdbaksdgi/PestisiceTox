150e9f248335de9d9b3c866989d63c8e0075f557
Flash <88150931+flashasdbaksdgi@users.noreply.github.com>
2021-08-14 19:12:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,3 @@
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import streamlit as st
 import pandas as pd
 from PIL import Image
